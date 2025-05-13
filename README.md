f5e24d4c3d4c74166819bcbb2605c7995fb8074e
BGNS42 <196232533+BGNS42@users.noreply.github.com>
2025-05-13 10:53:57 -0300

    CPU choice logic and Human choice logic

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # rockPaperAndScissors
 TOP Project: Rock paper and scissors from Foundations Course
+
+# Making a Rock Paper and Scissors game
+Game will be played against computer.
+
+The computer generate a random value between Rock, Paper or Scissors
+
+User choose between Rock, Paper and Scissors
+
+Compare Computer choice and User choice
+
+Rock beats Scissors
+Scissors beats Paper
+Paper beats Rock
--- src/script.js
+++ src/script.js
@@ -1,1 +1,31 @@
-console.log("hello world, lets start this project!")
+// Game will be played against computer.
+
+// The computer generate a random value between Rock, Paper or Scissors
+
+// User choose between Rock, Paper and Scissors
+
+// Compare Computer choice and User choice
+
+// Rock beats Scissors
+// Scissors beats Paper
+// Paper beats Rock
+
+
+// computer choice logic
+
+const choice = ["Rock", "Paper", "Scissors"];
+function getComputerChoice() {
+    let randomNum = Math.floor(Math.random()*3) + 1;
+    return choice[randomNum - 1];
+}
+
+// console.log(`CPU choice: ${getComputerChoice()}`);
+
+// human choice logic
+function getHumanChoice() {
+    let userChoice = prompt("Choose: Rock, Paper or Scissors?");
+    return userChoice;
+}
+
+
+// console.log(`Human Choice: ${getHumanChoice()}`);
